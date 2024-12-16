790049cb2f48b53885c375a0671e40399512037b
alejandracflores <114045083+alejandracflores@users.noreply.github.com>
2024-12-16 13:12:36 -0600

    initial commmit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-', // Añade un prefijo a las clases de Tailwind
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
